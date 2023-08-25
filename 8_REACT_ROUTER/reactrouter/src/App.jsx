fa9042da636f033299674f172382841c9b467c84
Herbert <herbertkayky9@gmail.com>
2023-08-25 15:38:14 -0300

    :sparkles: feat: page 404

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/info";
+import NotFound from "./pages/NotFound";
+
 // Components
 import Navbar from "./components/Navbar";
 
@@ -18,10 +20,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* Rota dinâmica */}
+          {/* 4 - Rota dinâmica */}
           <Route path="/products/:id" element={<Product />} />
-          {/* Nested Route */}
+          {/* 6 - Nested Route */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 7 - No Match Route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
