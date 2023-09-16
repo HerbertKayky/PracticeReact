eae6f73936e781080c9acd5dc17700b1f6d51863
Herbert <herbertkayky9@gmail.com>
2023-09-16 19:03:03 -0300

    :sparkles: feat: Register screen created

--- 10_MINIBLOG/miniblog/src/firebase/config.jsx
+++ 10_MINIBLOG/miniblog/src/firebase/config.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0VX3PhA1jNL6BAAOTvKzyhUXmuGg2zRE",
