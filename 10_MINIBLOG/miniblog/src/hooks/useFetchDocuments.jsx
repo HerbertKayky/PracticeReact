b638c5c81fa1c19ed427329c0ba4ab84b720f1f1
Herbert <herbertkayky9@gmail.com>
2023-10-28 19:10:05 -0300

    :sparkles: feat: Add data fetch hook

---
+++ 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+  QuerySnapshot,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docColection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // Deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docColection);
+
+      try {
+        let q;
+
+        // busca
+        // dashboard
+
+        q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+        await onSnapshot(q, (QuerySnapshot) => {
+          setDocuments(
+            QuerySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+  }, [docColection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+  return documents, loading, error;
+};
